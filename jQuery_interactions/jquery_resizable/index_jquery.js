5cb846dbd4b3b64541fe4293d4a2a760ecb212ae
abhishek121205 <khacharabhi3@gmail.com>
2024-08-10 11:54:16 +0530

    jquery interactions

---
+++ jQuery_interactions/jquery_resizable/index_jquery.js
@@ -0,0 +1,10 @@
+$("document").ready(function(){
+   $("#img").resizable({
+    // animate: true,
+    // containment: "parent",
+    maxHeight: 400,
+    minHeight: 100,
+    ghost: true,
+    // aspectRatio: true
+   })
+})
